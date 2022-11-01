84937ca757e3037d274868287bc2a040e55b321a
jonasbn <jonasbn@gmail.com>
2022-11-01 17:46:21 +0100

    Corrected title

--- packages/p/r/r.go
+++ packages/p/r/r.go
@@ -23,7 +23,7 @@ const tpl = `
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <title>CodePen - error, success, warning and alert Messages</title>
+    <title>pxy.fi</title>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"
