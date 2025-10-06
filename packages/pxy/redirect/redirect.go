0d39b9731acac554f0aa385dfe9960f07ea05c3a
jonasbn <jonasbn@gmail.com>
2025-10-06 18:26:21 +0200

    Minor clean up, let us see if deployment works as expected

--- packages/pxy/redirect/redirect.go
+++ packages/pxy/redirect/redirect.go
@@ -96,22 +96,22 @@ func createSafeErrorMessage(scheme, host, version, fragment string, messageType 
 }
 
 // Can be uncommented for local testing
-/* func main() {
+func main() {
 	args := make(map[string]interface{})
 	headers := make(map[string]interface{})
 
 	headers["user-agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko)"
 	headers["do-connecting-ip"] = "192.168.1.2"
-	headers["referer"] = "https://pxy.fi/p/r/4/wall"
+	headers["referer"] = "https://pxy.fi/p/r/4/wc++98-compat-bind-to-temporary-copy"
 	headers["x-request-id"] = "4d84db433a35256e7fdd395f430a9121"
 
-	args["__ow_path"] = "/4/wall"
+	args["__ow_path"] = "/4/wc++98-compat-bind-to-temporary-copy"
 	args["__ow_headers"] = headers
 
 	resp := Main(args)
 
 	fmt.Printf("Response: %#v\n", resp)
-} */
+}
 
 func Main(args map[string]interface{}) Response {
 
@@ -326,12 +326,12 @@ func emitHeartbeat() {
 	resp, err := client.Get(url)
 
 	if err != nil {
-		logger.Errorf("Unable to emit heartbeat: %s", err)
+		logger.Errorf("Unable to emit heartbeat to URL: %s, %s", url, err)
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		logger.Errorf("Emitted heartbeat failed: %s", resp.Status)
+		logger.Errorf("Emitting heartbeat failed: %s", resp.Status)
 	}
 }
